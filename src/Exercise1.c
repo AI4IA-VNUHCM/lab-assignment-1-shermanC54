0222fb61e8a42440a24353bad8140ec1429c8816
shermanC54 <ITITIU19007@student.hcmiu.edu.vn>
2021-10-04 15:22:49 +0700

    lab1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -16,6 +16,12 @@ int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	//Your codes here
+	int number;
+	while(testcase != 0){
+		number = testcase%10;
+		printf("%d  ",number);
+		testcase /= 10;
+	}
 	
 	return 0;
 }
